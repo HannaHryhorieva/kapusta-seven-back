9d56dcf9a780a9564c684a4e0c1ba7e5a073545b
RostyslavStihailoDev <rstihailo@methys.com>
2021-12-12 22:57:31 +0200

    Implement logic that changes user balance

--- controllers/transaction/create.js
+++ controllers/transaction/create.js
@@ -1,9 +1,19 @@
-const Transaction = require('../../model/transaction')
+const { Transaction, User } = require('../../model')
+const { BadRequest } = require('http-errors')
 
 module.exports = async (req, res, next) => {
-  const { _id } = req.user
+  const { _id, balance = 0 } = req.user
+  const { amount, isIncome } = req.body
+
+  if (!isIncome && amount > balance) {
+    next(new BadRequest('Amount is greater than balance'))
+  }
 
   const transaction = await new Transaction({ ...req.body, owner: _id })
+
+  await User.findByIdAndUpdate(_id, {
+    balance: isIncome ? balance + amount : balance - amount,
+  })
 
   await transaction.save()
 
--- controllers/transaction/remove.js
+++ controllers/transaction/remove.js
@@ -1,16 +1,24 @@
 const { NotFound } = require('http-errors')
-const Transaction = require('../../model/transaction')
+const { Transaction, User } = require('../../model')
 
 module.exports = async (req, res, next) => {
   const { id } = req.params
-  const { _id } = req.user
+  const { _id, balance } = req.user
 
-  const transaction = await Transaction.findOneAndDelete({
+  const transaction = await Transaction.findOne({
     _id: id,
     owner: _id,
   })
 
   if (!transaction) next(new NotFound('Transaction not found!'))
+
+  const { isIncome, amount } = transaction
+
+  await Transaction.findByIdAndRemove(id)
+
+  await User.findByIdAndUpdate(_id, {
+    balance: isIncome ? balance - amount : balance + amount,
+  })
 
   res.status(200).json({ message: 'Transaction deleted', data: transaction })
 }
