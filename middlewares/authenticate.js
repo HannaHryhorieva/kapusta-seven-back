bcacb0c03b69c316302f6a9239731c89af59211f
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-12-21 19:01:36 +0200

    autenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,39 +6,25 @@ const { User } = require('../model')
 const { SECRET_KEY } = process.env
 
 const authenticate = async (req, res, next) => {
-  const allUsers = await User.find()
   try {
     const [bearer, token] = req.headers.authorization.split(' ')
-    const userFind = allUsers.find(user => { return user.token === token ? user : null })
     if (bearer !== 'Bearer') {
       throw new Unauthorized('Not authorized')
     }
-    if (!userFind || userFind.token !== token) {
-      throw new Unauthorized('Invalid token')
-    }
-    // console.log(userFind.isGoogle)
-    if (!userFind.isGoogle) {
-      try {
-        const { id } = jwt.verify(token, SECRET_KEY)
-
-        const user = await User.findById(id)
-        console.log(user)
-        if (!user || !userFind) {
-          throw new NotFound('User not found!')
-        }
-        if (!user.token || user.token !== token) {
-          throw new Unauthorized('Invalid token')
-        }
-        req.user = user
-
-        next()
-      } catch (error) {
-        throw new Unauthorized('Invalid token')
+    try {
+      const { id } = jwt.verify(token, SECRET_KEY)
+      const user = await User.findById(id)
+      if (!user) {
+        throw new NotFound('User not found!')
       }
+      if (!user.token) {
+        throw new Unauthorized()
+      }
+      req.user = user
+      next()
+    } catch (error) {
+      throw new Unauthorized(error.message)
     }
-
-    req.user = userFind
-    next()
   } catch (error) {
     next(error)
   }
