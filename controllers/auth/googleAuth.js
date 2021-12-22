74763d2090f4b82ed0c000ffcc1d9875e798ef36
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-12-22 21:35:45 +0200

    commit

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -29,7 +29,7 @@ const googleRedirect = async (req, res, next) => {
     data: {
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.BASE_URL}/auth/google-redirect`,
+      redirect_uri: `${process.env.BASE_URL}auth/google-redirect`,
       grant_type: 'authorization_code',
       code,
     }
