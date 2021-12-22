2ce5ef7687bf7f24bf6618998ae0e5d4c858823b
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-12-22 12:31:12 +0200

    mailLink

--- public/mailVerify.js
+++ public/mailVerify.js
@@ -1,5 +1,5 @@
 const mailVerify = (verificationToken, name) => {
-  const urlVerify = `http://localhost:3001/auth/verify/${verificationToken}`
+  const urlVerify = `https://kapusta-seven.herokuapp.com/auth/verify/${verificationToken}`
   return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html
   xmlns="http://www.w3.org/1999/xhtml"
